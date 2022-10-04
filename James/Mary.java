61447d01adde1e01a8801afa8d2dd9f0113f69f5
Mazol Thomas Biem <biemtop718@gmail.com>
2022-10-04 19:34:55 +0300

    added int a,b,c,d,e

---
+++ James/Mary.java
@@ -0,0 +1,16 @@
+package James;
+
+public class Mary
+{
+    public static void main(String[]args)
+    {
+        int a = 1000;
+        int b = 2000;
+        int c = 3000;
+
+        int d = a + b;
+        int e = d * c;
+
+        System.out.println("The Final Multiplication Answer is: " + e);
+    }
+}
